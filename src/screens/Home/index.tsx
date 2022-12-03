6e181ed386e142b7b7cc0d354299e7ad0500f943
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-12-03 10:57:19 -0300

    New: configuração inicial do projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+import { Container } from "./styled";
+
+export function HomeScreen() {
+  return (
+    <Container>
+      <Text>HomeScreen</Text>
+    </Container>
+  );
+}
